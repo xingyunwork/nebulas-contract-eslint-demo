2eac60f3d46367eee9bbe622012f5b693dd82792
yucopowo <yucopowo@gmail.com>
2018-08-19 23:28:15 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": ["eslint:recommended","plugin:nebulas-contract/recommended"],
+    // "plugins": ["nebulas-contract"],
+    "parserOptions": {
+        "ecmaVersion": 2016,
+        "sourceType": "module"
+    },
+    "rules": {
+    }
+};
