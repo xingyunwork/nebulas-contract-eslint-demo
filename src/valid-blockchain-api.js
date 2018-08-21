08d279d3a35fbb0921f824fe399b04c7e541ef4f
杨修良 <yangxiuliang@moseeker.com>
2018-08-21 18:42:45 +0800

    fix config

---
+++ src/valid-blockchain-api.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var TestContract = function () {
+};
+
+TestContract.prototype = {
+    init: function () {
+        console.log('init: Blockchain.block.coinbase = ' + Blockchain.block.coinbase);
+        console.log('init: Blockchain.block.hash = ' + Blockchain.block.hash);
+        console.log('init: Blockchain.block.height = ' + Blockchain.block.height);
+        console.log('init: Blockchain.transaction.from = ' + Blockchain.transaction.from);
+        console.log('init: Blockchain.transaction.to = ' + Blockchain.transaction.to);
+        console.log('init: Blockchain.transaction.value = ' + Blockchain.transaction.value);
+        console.log('init: Blockchain.transaction.nonce = ' + Blockchain.transaction.nonce);
+        console.log('init: Blockchain.transaction.hash = ' + Blockchain.transaction.hash);
+
+
+
+        console.log(Blockchain.block1.nonce);
+
+    },
+};
+
+module.exports = TestContract;
