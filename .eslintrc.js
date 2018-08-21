1d8b18fdf51cf0a6ff193b683ed027a3dfe0d97f
杨修良 <yangxiuliang@moseeker.com>
2018-08-21 18:29:09 +0800

    fix config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,9 @@ module.exports = {
     "env": {
         "es6": true
     },
-    "extends": ["plugin:nebulas-contract/recommended"],
-    "plugins": ["nebulas-contract"],
+    // "extends": ["plugin:nebulas-contract/recommended"],
+    // "plugins": ["nebulas-contract"],
+    "extends": ["nebulas-contract"],
     "parserOptions": {
         "ecmaVersion": 2016,
         "sourceType": "module"
